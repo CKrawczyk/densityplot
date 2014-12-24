f9bbc505527ccd341b5ba8be2ee2a1c289d2a3eb
CKrawczyk <coleman.krawczyk@gmail.com>
2014-12-23 22:20:27 -0500

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='densityplot',
-      version='0.2',
+      version='1.0.0',
       description='tools to plot large numbers of scatter points',
       author='Dr. Coleman Krawczyk',
       author_email='coleman.krawczyk@gmail.com',
